6f2a5ca0255bcd7303402ceb2671d448f710969e
Simon Blom <102825244+simonblo@users.noreply.github.com>
2023-12-20 17:28:55 +0100

    Complete exercise 2

--- Exercise 2/Project/Main.cu
+++ Exercise 2/Project/Main.cu
@@ -8,31 +8,31 @@
 #define STRIDE 1048576
 #define STREAM true
 
-__global__ void gpuVectorAdd(float* bufferIn1, float* bufferIn2, float* bufferOut, int bufferSize)
+__global__ void KernelVectorAdd(float* input1, float* input2, float* output, int size)
 {
 	int tid = threadIdx.x + blockIdx.x * blockDim.x;
-	if (tid < bufferSize) bufferOut[tid] = bufferIn1[tid] + bufferIn2[tid];
+	if (tid < size) output[tid] = input1[tid] + input2[tid];
 }
 
-__global__ void gpuVectorAdd(float* bufferIn1, float* bufferIn2, float* bufferOut, int bufferSize, int bufferOffset)
+__global__ void KernelVectorAdd(float* input1, float* input2, float* output, int size, int offset)
 {
-	int tid = threadIdx.x + blockIdx.x * blockDim.x + bufferOffset;
-	if (tid < bufferSize) bufferOut[tid] = bufferIn1[tid] + bufferIn2[tid];
+	int tid = threadIdx.x + blockIdx.x * blockDim.x + offset;
+	if (tid < size) output[tid] = input1[tid] + input2[tid];
 }
 
-void VectorAdd(float* cpuBufferIn1, float* cpuBufferIn2, float* cpuBufferOut, float* gpuBufferIn1, float* gpuBufferIn2, float* gpuBufferOut)
+void VectorAdd(float* cpuInput1, float* cpuInput2, float* cpuOutput, float* gpuInput1, float* gpuInput2, float* gpuOutput)
 {
 	int threads = 64;
 	int blocks  = (BUFFER + threads - 1) / threads;
 
-	cudaMemcpy(gpuBufferIn1, cpuBufferIn1, BUFFER * sizeof(float), cudaMemcpyHostToDevice);
-	cudaMemcpy(gpuBufferIn2, cpuBufferIn2, BUFFER * sizeof(float), cudaMemcpyHostToDevice);
-	gpuVectorAdd<<<blocks, threads>>>(gpuBufferIn1, gpuBufferIn2, gpuBufferOut, BUFFER);
+	cudaMemcpy(gpuInput1, cpuInput1, BUFFER * sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(gpuInput2, cpuInput2, BUFFER * sizeof(float), cudaMemcpyHostToDevice);
+	KernelVectorAdd<<<blocks, threads>>>(gpuInput1, gpuInput2, gpuOutput, BUFFER);
 	cudaDeviceSynchronize();
-	cudaMemcpy(cpuBufferOut, gpuBufferOut, BUFFER * sizeof(float), cudaMemcpyDeviceToHost);
+	cudaMemcpy(cpuOutput, gpuOutput, BUFFER * sizeof(float), cudaMemcpyDeviceToHost);
 }
 
-void VectorAdd(float* cpuBufferIn1, float* cpuBufferIn2, float* cpuBufferOut, float* gpuBufferIn1, float* gpuBufferIn2, float* gpuBufferOut, cudaStream_t* stream)
+void VectorAdd(float* cpuInput1, float* cpuInput2, float* cpuOutput, float* gpuInput1, float* gpuInput2, float* gpuOutput, cudaStream_t* stream)
 {
 	int threads = 64;
 	int blocks  = (STRIDE + threads - 1) / threads;
@@ -41,14 +41,14 @@ void VectorAdd(float* cpuBufferIn1, float* cpuBufferIn2, float* cpuBufferOut, fl
 	// scale with the input size. This slows everything down since the GPU manages to complete all
 	// memory transfers before the CPU manages to complete the first kernel launch which causes the
 	// GPU to sit idle. We can make a small "dummy" launch at the start to minimize this latency.
-	gpuVectorAdd<<<1, 32>>>(gpuBufferIn1, gpuBufferIn2, gpuBufferOut, 0, 0);
+	KernelVectorAdd<<<1, 32>>>(gpuInput1, gpuInput2, gpuOutput, 0, 0);
 
 	for (int i = 0; i != BUFFER / STRIDE; ++i)
 	{
-		cudaMemcpyAsync(&gpuBufferIn1[i * STRIDE], &cpuBufferIn1[i * STRIDE], STRIDE * sizeof(float), cudaMemcpyHostToDevice, stream[i % 4]);
-		cudaMemcpyAsync(&gpuBufferIn2[i * STRIDE], &cpuBufferIn2[i * STRIDE], STRIDE * sizeof(float), cudaMemcpyHostToDevice, stream[i % 4]);
-		gpuVectorAdd<<<blocks, threads, 0, stream[i % 4]>>>(gpuBufferIn1, gpuBufferIn2, gpuBufferOut, BUFFER, i * STRIDE);
-		cudaMemcpyAsync(&cpuBufferOut[i * STRIDE], &gpuBufferOut[i * STRIDE], STRIDE * sizeof(float), cudaMemcpyDeviceToHost, stream[i % 4]);
+		cudaMemcpyAsync(&gpuInput1[i * STRIDE], &cpuInput1[i * STRIDE], STRIDE * sizeof(float), cudaMemcpyHostToDevice, stream[i % 4]);
+		cudaMemcpyAsync(&gpuInput2[i * STRIDE], &cpuInput2[i * STRIDE], STRIDE * sizeof(float), cudaMemcpyHostToDevice, stream[i % 4]);
+		KernelVectorAdd<<<blocks, threads, 0, stream[i % 4]>>>(gpuInput1, gpuInput2, gpuOutput, BUFFER, i * STRIDE);
+		cudaMemcpyAsync(&cpuOutput[i * STRIDE], &gpuOutput[i * STRIDE], STRIDE * sizeof(float), cudaMemcpyDeviceToHost, stream[i % 4]);
 	}
 
 	cudaDeviceSynchronize();
@@ -63,12 +63,12 @@ double GetSeconds()
 
 int main()
 {
-	float* cpuBufferIn1;
-	float* cpuBufferIn2;
-	float* cpuBufferOut;
-	float* gpuBufferIn1;
-	float* gpuBufferIn2;
-	float* gpuBufferOut;
+	float* cpuInput1;
+	float* cpuInput2;
+	float* cpuOutput;
+	float* gpuInput1;
+	float* gpuInput2;
+	float* gpuOutput;
 
 	cudaStream_t stream[4];
 	cudaStreamCreate(&stream[0]);
@@ -76,25 +76,25 @@ int main()
 	cudaStreamCreate(&stream[2]);
 	cudaStreamCreate(&stream[3]);
 
-	cudaHostAlloc((void**)&cpuBufferIn1, BUFFER * sizeof(float), cudaHostAllocDefault);
-	cudaHostAlloc((void**)&cpuBufferIn2, BUFFER * sizeof(float), cudaHostAllocDefault);
-	cudaHostAlloc((void**)&cpuBufferOut, BUFFER * sizeof(float), cudaHostAllocDefault);
+	cudaHostAlloc((void**)&cpuInput1, BUFFER * sizeof(float), cudaHostAllocDefault);
+	cudaHostAlloc((void**)&cpuInput2, BUFFER * sizeof(float), cudaHostAllocDefault);
+	cudaHostAlloc((void**)&cpuOutput, BUFFER * sizeof(float), cudaHostAllocDefault);
 
-	cudaMalloc((void**)&gpuBufferIn1, BUFFER * sizeof(float));
-	cudaMalloc((void**)&gpuBufferIn2, BUFFER * sizeof(float));
-	cudaMalloc((void**)&gpuBufferOut, BUFFER * sizeof(float));
+	cudaMalloc((void**)&gpuInput1, BUFFER * sizeof(float));
+	cudaMalloc((void**)&gpuInput2, BUFFER * sizeof(float));
+	cudaMalloc((void**)&gpuOutput, BUFFER * sizeof(float));
 
 	srand(time(NULL));
 
 	for (int i = 0; i != BUFFER; ++i)
 	{
-		cpuBufferIn1[i] = (float)rand() / (float)RAND_MAX;
-		cpuBufferIn2[i] = (float)rand() / (float)RAND_MAX;
+		cpuInput1[i] = (float)rand() / (float)RAND_MAX;
+		cpuInput2[i] = (float)rand() / (float)RAND_MAX;
 	}
 
 	double time0 = GetSeconds();
 	#if STREAM
-	VectorAdd(cpuBufferIn1, cpuBufferIn2, cpuBufferOut, gpuBufferIn1, gpuBufferIn2, gpuBufferOut, stream);
+	VectorAdd(cpuInput1, cpuInput2, cpuOutput, gpuInput1, gpuInput2, gpuOutput, stream);
 	#else
 	VectorAdd(cpuBufferIn1, cpuBufferIn2, cpuBufferOut, gpuBufferIn1, gpuBufferIn2, gpuBufferOut);
 	#endif
@@ -104,20 +104,20 @@ int main()
 
 	for (int i = 0; i != BUFFER; ++i)
 	{
-		errorCount += ((cpuBufferIn1[i] + cpuBufferIn2[i]) != cpuBufferOut[i]);
+		errorCount += ((cpuInput1[i] + cpuInput2[i]) != cpuOutput[i]);
 	}
 
 	printf("Elements: %d\n", BUFFER);
 	printf("Errors:   %d\n", errorCount);
 	printf("Time:     %f\n", time1 - time0);
 
-	cudaFree(gpuBufferIn1);
-	cudaFree(gpuBufferIn2);
-	cudaFree(gpuBufferOut);
+	cudaFree(gpuInput1);
+	cudaFree(gpuInput2);
+	cudaFree(gpuOutput);
 
-	cudaFreeHost(cpuBufferIn1);
-	cudaFreeHost(cpuBufferIn2);
-	cudaFreeHost(cpuBufferOut);
+	cudaFreeHost(cpuInput1);
+	cudaFreeHost(cpuInput2);
+	cudaFreeHost(cpuOutput);
 
 	cudaStreamDestroy(stream[0]);
 	cudaStreamDestroy(stream[1]);
